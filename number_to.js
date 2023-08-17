3f73e31145125a4be165c5a80d49b8b2db87adef
priyanshi <priyanshisharma1048@gmail.com>
2023-08-17 21:35:04 +0530

    java

---
+++ number_to.js
@@ -0,0 +1,9 @@
+// 4. print value from 1 to 10 and if value is 5 so it will print ("70") otherwise print the value
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+      console.log("70");
+    } else {
+      console.log(i);
+    }
+  }
+  

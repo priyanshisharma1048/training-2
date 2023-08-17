1d25d173cc43b299cdf705747a7af4ed5ebaa25e
priyanshi <priyanshisharma1048@gmail.com>
2023-08-17 21:36:45 +0530

    numbers

---
+++ numbers_to.js
@@ -0,0 +1,9 @@
+// 5. print value from 1 to 10 and if value is 5 so it will print ("priyanshi") otherwise print the value
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+      console.log("priyanshi");
+    } else {
+      console.log(i);
+    }
+  }
+  
